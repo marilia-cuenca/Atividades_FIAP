14fcba4be6db69255ec96af77152c354e93ff08e
Marília Cuenca <82479792+marilia-cuenca@users.noreply.github.com>
2021-10-02 15:45:11 -0300

    Adicionando os enunciados

--- RM92388_EX03_Cap02_extra.py
+++ RM92388_EX03_Cap02_extra.py
@@ -1,3 +1,9 @@
+"""Hora de decidir! Os colaboradores da sua equipe foram sorteados para ganharem um console de última geração cada um por conta do bom desempenho que tiveram nos últimos projetos. Por uma questão de logística, porém, a empresa pede que todos os 5 membros da equipe recebam o mesmo aparelho.
+
+Crie um algoritmo onde o usuário possa digitar o voto de cada um dos 5 membros da equipe e, ao final, exiba qual foi o console escolhido e com quantos votos.
+
+As opções são: PLAYSTATION, XBOX e NINTENDO."""
+
 voto1 = int(input("Digite o 1º voto \n[1]-Playstation; [2]-XBOX; [3]-Nintendo: "))
 voto2 = int(input("Digite o 2º voto \n[1]-Playstation; [2]-XBOX; [3]-Nintendo: "))
 voto3 = int(input("Digite o 3º voto \n[1]-Playstation; [2]-XBOX; [3]-Nintendo: "))
